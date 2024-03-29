72a73e658c23575fc8f0ad61922b30b1a2ca2f94
McFlanky <cturjanica02@gmail.com>
2024-03-29 11:07:53 -0400

    added mapb command to go back in pages to add pagination to location areas

--- command_map.go
+++ command_map.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
 
 func callbackMap(cfg *config) error {
 	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Location Areas:")
+	for _, area := range resp.Results {
+		fmt.Printf(" - %s\n", area.Name)
+	}
+	cfg.nextLocationAreaURL = resp.Next
+	cfg.prevLocationAreaURL = resp.Previous
+	return nil
+}
+
+func callbackMapb(cfg *config) error {
+	if cfg.prevLocationAreaURL == nil {
+		return errors.New("first page reached")
+	}
+	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.prevLocationAreaURL)
 	if err != nil {
 		log.Fatal(err)
 	}
